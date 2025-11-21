d2c95a32711f840559518085986c348c7b48b161
Caio Pereira <caio.pereira@ebury.com>
2025-11-21 00:13:23 -0300

    initial commit

---
+++ src/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Request struct {
+	WebhookURL string  `json:"webhook_url" binding:"required"`
+	Amount     float64 `json:"amount" binding:"required"`
+	Status     string  `json:"status" binding:"required"`
+}
+
+type WebhookPayload struct {
+	PaymentID string `json:"payment_id"`
+	Status    string `json:"status"`
+}
+
+func main() {
+	app := gin.Default()
+
+	app.POST("/payments", func(ctx *gin.Context) {
+		var request Request
+		if err := ctx.ShouldBindJSON(&request); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		paymentID := generatePaymentID()
+
+		go triggerWebhook(request.WebhookURL, paymentID, request.Status)
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"payment_id":  paymentID,
+			"webhook_url": request.WebhookURL,
+			"status":      request.Status,
+			"amount":      request.Amount,
+		})
+	})
+
+	if err := app.Run(":4003"); err != nil {
+		log.Fatalf("could not initialize HTTP server: %s", err)
+	}
+}
+
+func triggerWebhook(url, paymentID, status string) {
+	time.Sleep(10 * time.Second)
+
+	payload := WebhookPayload{
+		PaymentID: paymentID,
+		Status:    status,
+	}
+
+	body, _ := json.Marshal(payload)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		log.Println("erro ao enviar webhook:", err)
+		return
+	}
+
+	rawResponse, _ := io.ReadAll(resp.Body)
+
+	log.Printf("webhook enviado. HTTP Status Code: %d Response: %s", resp.StatusCode, string(rawResponse))
+}
+
+func generatePaymentID() string {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	return time.Now().Format("20060102150405")
+}
